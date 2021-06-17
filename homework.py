7fd1cf1667cfca8b5e16d1d52999826eb102278e
sshovgurov <rdigga99@gmail.com>
2021-06-18 01:54:56 +0300

    api

--- homework.py
+++ homework.py
@@ -2,7 +2,6 @@ import os
 import time
 import logging
 import requests
-import telegram
 from dotenv import load_dotenv
 from telegram import Bot
 
@@ -18,7 +17,7 @@ PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 API = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-bot = Bot(token = TELEGRAM_TOKEN)
+bot = Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
@@ -27,15 +26,14 @@ def parse_homework_status(homework):
     if status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict='Ревьюеру всё понравилось, можно приступать к следующему уроку.'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_date):
     headers = {'Authorization': f'OAuth {TELEGRAM_TOKEN}'}
     params = {'from_date': current_date}
-    homework_statuses = requests.get(API, headers=headers,
-    params=params)
+    homework_statuses = requests.get(API,headers=headers,params=params)
     return homework_statuses.json()
 
 
@@ -44,15 +42,17 @@ def send_message(message, bot_client):
 
 
 def main():
-    bot = Bot(token = TELEGRAM_TOKEN)
     current_timestamp = int(time.time())  # начальное значение timestamp
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
+                send_message(parse_homework_status(
+                    new_homework.get('homeworks')[0])
+                )
+            current_timestamp = new_homework.get('current_date', 
+                                                 current_timestamp)
             time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
